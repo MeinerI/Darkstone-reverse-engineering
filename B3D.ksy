7f13ba03af2cf97c7ecf8afc196ea580a5a67fdc
Man Sun <ramfanderu@mail.ru>
2020-05-08 12:59:10 +0300

    Add files via upload

---
+++ B3D.ksy
@@ -0,0 +1,99 @@
+meta:
+  id: b3d
+  file-extension: b3d
+  endian: le
+
+#############################################
+
+seq:
+  - id: block_count # количество саб-мешей
+    type: u4
+
+  - id: blocks 
+    type: block
+    repeat: expr
+    repeat-expr: block_count
+
+#############################################
+
+types:
+
+  block:
+    seq:
+
+    - id: one
+      type: u4
+
+    - id: str3df
+      type: str
+      size: 8
+      encoding: ASCII
+
+    - id: string48s
+      type: str
+      size: 48
+      encoding: ASCII
+    
+    - id: floats6
+      type: f4
+      repeat: expr
+      repeat-expr: 6
+
+    - id: vertex_count # количество вершин
+      type: u4
+
+    - id: face_count # граней 
+      type: u4
+
+    - id: face_count_2
+      type: u4
+
+    - id: vertex_count_2
+      type: u4
+
+    - id: coords
+      type: vector
+      repeat: expr
+      repeat-expr: vertex_count
+
+    - id: polygon
+      type: float8
+      repeat: expr
+      repeat-expr: face_count
+
+#############################################
+
+  vector:
+    seq:
+      - id: x
+        type: f4
+      - id: y
+        type: f4
+      - id: z
+        type: f4
+
+#############################################
+
+  float8:
+    seq:
+      - id: color # цвет ? (как в o3d)
+        type: u1
+        repeat: expr
+        repeat-expr: 4
+
+      - id: texture_coord # для 6 для tri, 8 для quad ? (как в o3d)
+        type: f4
+        repeat: expr
+        repeat-expr: 8
+
+      - id: face3or4 # tri (65535) or quad
+        type: u2
+        repeat: expr
+        repeat-expr: 4
+
+      - id: hz_array # 6 байт окончания / в конце может текстура, как в o3d ?
+        type: u2
+        repeat: expr
+        repeat-expr: 3
+
+#############################################
---
+++ b3d_2_obj.cs
@@ -0,0 +1,123 @@
+// ôàéëû GOBELIN, TEMPLE6, TOWN ñ îøèáêàìè â êîëè÷åñòâå ñàáìåøåé è âåğøèí
+
+//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
+  using System; using System.IO; using System.Linq; using System.Text; using System.Collections; 
+  using System.Collections.Generic; using System.Text.RegularExpressions; using System.Globalization; 
+//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
+
+sealed class Test
+{
+    static uint vertexCount; 
+    static uint faceCount;
+
+    public static void Main()
+    {
+        SearchOption SOAD = SearchOption.AllDirectories;
+        string[] filesName = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.b3d",  SOAD); 
+        System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+        foreach (var fileIn in filesName)
+        {
+						using (BinaryReader br = new BinaryReader(File.Open(fileIn, FileMode.Open)))
+						{
+                int submesh_count = br.ReadInt32(); 
+                    
+                for (int o = 0; o < submesh_count; o++)
+                {
+                    using (StreamWriter sw = new StreamWriter(fileIn + o + ".obj"))
+                    {
+                        O3dModel o3d = new O3dModel();
+
+                        uint type = br.ReadUInt32();  
+
+                        br.ReadBytes(8); 
+                        br.ReadBytes(48); 
+                        for(int i = 0; i < 6; i++) br.ReadSingle(); 
+                        
+                        vertexCount = br.ReadUInt32(); 
+                        faceCount   = br.ReadUInt32(); 
+                        uint faceCount2   = br.ReadUInt32(); 
+                        uint vertexCount1 = br.ReadUInt32(); 
+
+                        for(int i = 0; i < vertexCount; i++) 
+                        {
+                            o3d_vertex newv = new o3d_vertex();
+                            newv.x = br.ReadSingle();
+                            newv.y = br.ReadSingle();
+                            newv.z = br.ReadSingle();
+                            o3d.vertexes.Add(newv);
+                        }
+
+                        for(int i = 0; i < faceCount; i++) 
+                        {
+                            o3d_face newf = new o3d_face();
+
+                            br.ReadByte();
+                            br.ReadByte();
+                            br.ReadByte();
+                            br.ReadByte();
+
+                            br.ReadSingle();
+                            br.ReadSingle();
+                            br.ReadSingle();
+                            br.ReadSingle();
+                            br.ReadSingle();
+                            br.ReadSingle();
+                            br.ReadSingle();
+                            br.ReadSingle();
+
+                            for (int ii = 0; ii < 4; ii++) 
+                            {
+                                newf.index[ii] = br.ReadUInt16();
+                            }
+                            o3d.faces.Add(newf); 
+                            
+                            br.ReadUInt16();
+                            br.ReadUInt16();
+                            br.ReadUInt16();
+                        }
+
+// ======================================================== 
+
+                        foreach (var v in o3d.vertexes)
+                        sw.WriteLine("v " + v.x + " " + v.y + " " + v.z);
+
+                        var enumerator = o3d.faces.GetEnumerator();
+
+                        foreach (var v in o3d.faces)
+                        {
+                            if (v.index[0] == 65535 || v.index[1] == 65535 || v.index[2] == 65535 ) enumerator.MoveNext();
+                            sw.WriteLine("f " + (v.index[0]+1) + " " + (v.index[1]+1) + " " + (v.index[2]+1));
+                            if (v.index[3] != 65535) sw.WriteLine("f " + (v.index[0]+1) + " " + (v.index[2]+1) + " " + (v.index[3]+1));
+                        }
+                    
+                    }
+                }
+            }
+        } // foreach (var file in filesName)
+    } // public static void Main()
+} // sealed class Test {
+
+//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
+//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
+//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
+
+public class O3dModel
+{
+    public List<o3d_vertex> vertexes = new List<o3d_vertex>();
+    public List<o3d_face>   faces = new List<o3d_face>();
+}
+
+// ======================================================== 
+
+public class o3d_vertex 
+{
+  public float x, y, z; // Model vertex position (XYZ)
+}
+
+// ======================================================== 
+ 
+public class o3d_face // ãğàíü/ïîëèãîí (òğåóãîëüíèê èëè ÷åòûğåõóãîëüíèê)
+{
+  public ushort[] index = new ushort[4];
+}
